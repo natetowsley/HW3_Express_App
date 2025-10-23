96590189b54dce3f9351d841a65018cbb850c6ae
Nate Towsley <natetowsley@gmail.com>
2025-10-22 17:44:15 -0700

    Created /weather path, installed npm package 'cities'

--- index.mjs
+++ index.mjs
@@ -1,5 +1,6 @@
 import express from 'express';
 
+const cities = (await import('cities')).default;
 const app = express();
 
 app.set("view engine", "ejs");
@@ -8,6 +9,15 @@ app.use(express.static("public"));
 // root route
 app.get('/', (req, res) => {
     res.render('home.ejs');
+});
+
+app.get('/weather', async(req, res) => {
+    let zip = req.query.zip;
+    let city = cities.zip_lookup(zip);
+
+    res.render('weather.ejs', {city, zip});
+    console.log(city.city);
+    console.log(zip);
 });
 
 // start web server
---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+document.querySelector("#searchBtn").addEventListener("click", getData);
+
+async function getData() {
+    let zip = document.querySelector("#cityInput").value;
+    window.location.href = "/weather?zip=" + zip;
+}
