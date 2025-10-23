1ebe0aff4d38599e330c95bc008bcd7b6b3bd1d4
natetowsley <natetowsley@gmail.com>
2025-10-22 21:36:06 -0700

    weather.ejs begun, invalid.ejs begun along with route. Added css, home.ejs almost done.

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 import express from 'express';
 
-const KEY = "1615e43df7fb9f0e01249749512bfd86"
+const KEY = "1615e43df7fb9f0e01249749512bfd86";
 const cities = (await import('cities')).default;
 const app = express();
 
@@ -12,13 +12,24 @@ app.get('/', (req, res) => {
     res.render('home.ejs');
 });
 
+// weather route
 app.get('/weather', async(req, res) => {
     let zip = req.query.zip;
-    let city = cities.zip_lookup(zip);
-    let url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=${KEY}`;
-    res.render('weather.ejs', {city, zip});
-    console.log(city.city);
     console.log(zip);
+    let city = cities.zip_lookup(zip);
+    if (city == undefined) {
+        res.redirect(`/invalid`); // stack overflow
+    }
+    console.log(city);
+    let url = `https://api.openweathermap.org/data/3.0/onecall?lat=${city.latitude}&lon=${city.longitude}&exclude=minutely,alerts,daily&units=imperial&appid=${KEY}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    res.render('weather.ejs', {city, data});
+});
+
+app.get('/invalid', (req, res) => {
+    res.render('invalid.ejs');
 });
 
 // start web server
