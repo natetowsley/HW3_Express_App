21ac31fa121b9a797c553ad78a3019cc17ec3e62
Nate Towsley <natetowsley@gmail.com>
2025-10-22 18:51:10 -0700

    Working on weather.ejs, added script to button, working on /weather route with API

--- index.mjs
+++ index.mjs
@@ -1,5 +1,6 @@
 import express from 'express';
 
+const KEY = "1615e43df7fb9f0e01249749512bfd86"
 const cities = (await import('cities')).default;
 const app = express();
 
@@ -14,7 +15,7 @@ app.get('/', (req, res) => {
 app.get('/weather', async(req, res) => {
     let zip = req.query.zip;
     let city = cities.zip_lookup(zip);
-
+    let url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=${KEY}`;
     res.render('weather.ejs', {city, zip});
     console.log(city.city);
     console.log(zip);
--- public/js/script.js
+++ public/js/script.js
@@ -2,5 +2,5 @@ document.querySelector("#searchBtn").addEventListener("click", getData);
 
 async function getData() {
     let zip = document.querySelector("#cityInput").value;
-    window.location.href = "/weather?zip=" + zip;
+    window.location.href = "/weather?zip=" + zip; // stack overflow
 }
